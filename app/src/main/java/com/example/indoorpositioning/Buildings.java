dc17cc76fbb70a55882f59ac17c891bfee0dcb14
Ajnas <mailajnaskt@gmail.com>
2015-04-16 01:50:17 +0530

    Buildings

--- app/src/main/java/com/example/indoorpositioning/Buildings.java
+++ app/src/main/java/com/example/indoorpositioning/Buildings.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,6 +32,29 @@ public class Buildings extends Activity {
 		db=new DatabaseHelper(this);
 		add=(Button) findViewById(R.id.add);
 		buildingName= (EditText) findViewById(R.id.name);
+        buildingName.addTextChangedListener(new TextWatcher() {
+
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                if(charSequence.equals("")){
+                    add.setEnabled(false);
+                }
+                else
+                    add.setEnabled(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        buildingName.setText("");
 
 		add.setOnClickListener(new View.OnClickListener() {
 			
@@ -38,26 +63,18 @@ public class Buildings extends Activity {
 				String input=buildingName.getText().toString();
 				Intent intent=new Intent(getApplicationContext(),Positions.class);
 				intent.putExtra("BUILDING_NAME",input);
-				startActivityForResult(intent,0);
+				startActivity(intent);
 
 			}
 		});
 		buildings = db.getBuildings();
-
-
-		// Get the reference of ListViewAnimals
 		buildingsList = (ListView) findViewById(R.id.buildingslist);
-		
-	
-		
-		// Create The Adapter with passing ArrayList as 3rd parameter
-		 arrayAdapter = new ArrayAdapter<String>(this,
+		arrayAdapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1,buildings);
-		// Set The Adapter
+
 		buildingsList.setAdapter(arrayAdapter);
 		buildingsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 		    public void onItemClick(AdapterView parent, View v, int position, long id){
-		        // Start your Activity according to the item just clicked.
 		    String selectedBuilding=(String)parent.getItemAtPosition(position);
 		    Intent intent=new Intent(getApplicationContext(),Positions.class);
 			intent.putExtra("BUILDING_NAME",selectedBuilding);
@@ -92,16 +109,7 @@ public class Buildings extends Activity {
 		
 
 	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		buildings=db.getBuildings();
-		arrayAdapter = new ArrayAdapter<String>(this,
-					android.R.layout.simple_list_item_1,buildings);
-	
-		buildingsList.setAdapter(arrayAdapter);
-		super.onActivityResult(requestCode, resultCode, data);
-	}
+
 	
 }
 
