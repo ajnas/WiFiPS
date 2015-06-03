36f3f776d85a746ad7c711a52eed7523f3a1909f
Ajnas <mailajnaskt@gmail.com>
2015-06-03 14:19:08 +0530

    Minor UI changes

--- app/src/main/java/com/example/indoorpositioning/PositionData.java
+++ app/src/main/java/com/example/indoorpositioning/PositionData.java
@@ -11,7 +11,7 @@ import android.util.Log;
 public class PositionData implements Serializable {
 	public static final int MAX_DISTANCE=99999999;
 	private String name;
-	public static final int MINIMUM_COMMON_ROUTERS=1;
+	public static final int MINIMUM_COMMON_ROUTERS=3;
 	public HashMap<String, Integer> values;
     public HashMap<String,String> routers;
 	public PositionData(String name) {
